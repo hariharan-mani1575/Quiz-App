dd777e2c569213b24ca2c5be0ea76acd9908db13
Hariharan J <126954613+hariharan-mani1575@users.noreply.github.com>
2023-09-25 14:04:31 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Quiz from "./components/Quiz/Quiz";
+import { jsQuizz } from "./constants";
+
+
+function App() {
+  
+  return <Quiz questions={jsQuizz.questions} />;
+}
+
+export default App;
